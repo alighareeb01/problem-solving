35b20fc48956661decd80a24a62b226ffe2c6268
alighareeb01 <ghareeb874@gmail.com>
2026-01-17 21:06:49 +0200

    Container with most water leet code

---
+++ leetcode-problem-solving/Container With Most Water/ContainerWithMostWater.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let left = 0;
+    let right = height.length - 1;
+    let max = 0;
+    while(left < right)
+    {
+    let W = right - left;
+      let H = Math.min(height[left],  height[right]);
+      let area = W * H;
+      max = Math.max(max,area);
+      if(height[left]<  height[right])
+      {
+        left++;
+      }
+      else
+      {
+        right--;
+      }
+    }
+    return max;
+};
