9d6824a9cb785aec435ed8d3b3a6488b91e3b81b
alighareeb01 <ghareeb874@gmail.com>
2026-01-24 20:23:43 +0200

    solved StrStr

---
+++ leetcode-problem-solving/the Index of the First Occurrence in a String/StrStr.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */ var strStr = function (haystack, needle) {
+  //return haystack.indexOf(needle);
+  if (needle === "") return 0;
+
+  for (let i = 0; i <= haystack.length - needle.length; i++) {
+    let j = 0;
+
+    while (j < needle.length && haystack[i + j] === needle[j]) {
+      j++;
+    }
+
+    if (j === needle.length) {
+      return i;
+    }
+  }
+
+  return -1;
+};
