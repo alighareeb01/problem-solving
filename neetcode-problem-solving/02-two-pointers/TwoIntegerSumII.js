b1a5902b9e537a208a1bb1b5efb577e8409db898
alighareeb01 <ghareeb874@gmail.com>
2026-04-30 14:06:56 +0300

    two sum || input array is sorted

--- neetcode-problem-solving/02-two-pointers/TwoIntegerSumII.js
+++ neetcode-problem-solving/02-two-pointers/TwoIntegerSumII.js
@@ -5,29 +5,20 @@ class Solution {
    * @return {number[]}
    */
   twoSum(numbers, target) {
-    /*
-        for(let i = 0; i < numbers.length;i++)
-        {
-            for(let j = i +1;j <numbers.length;j++)
-            {
-                if(numbers[i] + numbers[j] === target){
-                    return [i+1,j+1];
-                }
-            }
-        }
-    }
-    */
-    let left = 0,
-      right = numbers.length - 1;
+    let left = 0;
+    let right = numbers.length - 1;
+
     while (left < right) {
-      let sum = numbers[left] + numbers[right];
+      const sum = numbers[left] + numbers[right];
+
       if (sum === target) {
         return [left + 1, right + 1];
-      } else if (sum > target) {
-        right--;
-      } else {
+      } else if (sum < target) {
         left++;
+      } else {
+        right--;
       }
     }
+    return [];
   }
 }
