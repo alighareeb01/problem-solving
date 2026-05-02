a8eea22237034539dfafa6a4d287f12de903fb6c
alighareeb01 <ghareeb874@gmail.com>
2026-05-02 17:07:08 +0300

    three sum

--- neetcode-problem-solving/02-two-pointers/ThreeSum.js
+++ neetcode-problem-solving/02-two-pointers/ThreeSum.js
@@ -1,41 +1,57 @@
 class Solution {
-    /**
-     * @param {number[]} nums
-     * @return {number[][]}
-     */
-    threeSum(nums) {
-        let result = [];
+  /**
+   * @param {number[]} nums
+   * @return {number[][]}
+   */
+  twoSum(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+    const res = [];
 
-        nums.sort((a,b)=>a-b);
+    while (left < right) {
+      const x = nums[left] + nums[right];
 
-        for(let i = 0; i < nums.length - 2; i++)
-        {
-            if(i > 0 && nums[i] === nums[i - 1]) continue;
-            let left  = i + 1;
-            let right = nums.length - 1;
+      if (x < target) {
+        left++;
+      } else if (x > target) {
+        right--;
+      } else {
+        res.push([nums[left], nums[right]]);
 
-            while(left < right)
-            {
-                let sum = nums[i] + nums[left] + nums[right];
-                if(sum === 0)
-                {
-                    result.push([nums[i],nums[left],nums[right]]);
-                    while(left < right && nums[left] === nums[left + 1]) left++;
-                    while(left < right && nums[right] === nums[right - 1]) right--;
+        while (left < right && nums[left] === nums[left + 1]) left++;
+        while (left < right && nums[right] === nums[right - 1]) right--;
 
-                    right--;
-                    left++;
-                }
-                else if(sum < 0)
-                {
-                    left++;
-                }
-                else
-                {
-                    right--;
-                }
-            }
-        }
-        return result;
+        left++;
+        right--;
+      }
     }
+    return res;
+  }
+  getRest(nums, i) {
+    return nums.slice(i + 1);
+  }
+  threeSum(nums) {
+    const arr = [];
+    const set = new Set();
+    const sortedNums = nums.sort((a, b) => a - b);
+
+    for (let i = 0; i < nums.length; i++) {
+      const target = sortedNums[i];
+
+      const rest = this.getRest(sortedNums, i);
+
+      let x = this.twoSum(rest, -target);
+      for (let el of x) {
+        const triplet = [target, ...el].sort((a, b) => a - b);
+
+        const key = triplet.join(",");
+
+        if (!set.has(key)) {
+          set.add(key);
+          arr.push(triplet);
+        }
+      }
+    }
+    return arr;
+  }
 }
