87e0ec9252d07ce557769052bdd35094dcaabb92
alighareeb01 <ghareeb874@gmail.com>
2026-05-04 00:49:59 +0300

    ContainerWithMostWater

--- neetcode-problem-solving/02-two-pointers/ContainerWithMostWater.js
+++ neetcode-problem-solving/02-two-pointers/ContainerWithMostWater.js
@@ -7,11 +7,10 @@ class Solution {
     let left = 0;
     let right = heights.length - 1;
     let max = 0;
+
     while (left < right) {
-      let width = right - left;
-      let height = Math.min(heights[left], heights[right]);
-      let area = width * height;
-      max = Math.max(max, area);
+      let area = Math.min(heights[left], heights[right]) * (right - left);
+      max = Math.max(area, max);
       if (heights[left] < heights[right]) {
         left++;
       } else {
